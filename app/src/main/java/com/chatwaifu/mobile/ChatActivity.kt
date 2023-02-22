d6c9ef7d0c276527ccb0ba9cfc4942a9f2c43f65
Voine <axion9336@gmail.com>
2023-02-23 00:18:57 +0800

    remove default key....

--- app/src/main/java/com/chatwaifu/mobile/ChatActivity.kt
+++ app/src/main/java/com/chatwaifu/mobile/ChatActivity.kt
@@ -47,7 +47,6 @@ class ChatActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             chatViewModel.initYuuka()
         }
-        chatViewModel.setBaiduTranslate("20230220001569033","yF7uQIuBYgY13SfnbY3U")
     }
 
     private fun showBaseFragment() {
